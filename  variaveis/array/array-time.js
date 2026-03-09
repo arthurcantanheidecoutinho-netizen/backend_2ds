642fe554baee29a42a450260e053b4c7a98c9b5f
arthurcantanheidecoutinho-netizen <arthur.cantanheide.coutinho@escola.pr.gov.br>
2026-03-09 13:28:42 +0000

    introdução aos Arrays em JS

---
+++  variaveis/array/array-time.js
@@ -0,0 +1,9 @@
+// Array: são agrupamentos de valores em forma de lista
+// são indexados a partir de ZERO até o infinito 
+// podem ter valores homogeneos/heterogeneos
+// possuem métodos (funções) própias
+
+const time = ['Petter', 'Mariah']
+ // indeices     0         1 
+
+ console.log(time[1])
---
+++  variaveis/var-pessoa.js
@@ -0,0 +1,20 @@
+// , var, let, const
+// tipos de dados
+
+let nome = "Petter"   // string
+let sobrenome = "Parker"  // string
+const cpf = "88888888-98"  // string
+const rg = "132131 ssp"    //string
+let idade = 22              // number
+let solteiro = true           // boolean
+let nota =  5 + 3               //number
+
+console.log(nome )
+console.log(sobrenome )
+console.log(cpf )
+console.log(rg )
+console.log(idade )
+console.log(solteiro )
+console.log(nota )
+
+ 
