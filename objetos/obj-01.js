57daab913d3378794f29399d1cad41776f14ace9
arthurcantanheidecoutinho-netizen <arthur.cantanheide.coutinho@escola.pr.gov.br>
2026-03-25 11:37:16 +0000

    finalização de Objetos JS

---
+++ objetos/obj-01.js
@@ -0,0 +1,15 @@
+// Objetos são estruturas chave-valor
+// Também chamadas "Key-value"
+// Objetos podem conter: arrays, string,
+// number, objects, functions...
+// possuem métodos própios
+
+const jogador = {
+    nome: "Coutinho",
+    gols: 450,
+    times: ['Palmeiras', 'Coxa', 'Flamengo', 'Corinthians']
+}
+console.log(jogador.nome)
+console.log(jogador.gols)
+console.log(jogador.times)
+
---
+++ objetos/obj-02.js
@@ -0,0 +1,19 @@
+// A partir do Objeto de exemplo crie:
+// Um objeto com o tema á sua escola
+// deve possuir no mínimo 6 propiedades
+// imprima de um jeito amigável:
+// Ex.: console.log("Cor do veículo: " + carro.cor)
+
+const porshe = {
+    nome: porshe,
+    cor: [branco],
+    valor: 400.000
+
+}
+console.log("nome do carro + porshe.nome")
+console.log("Cor do carro: " + porshe.cor)
+console.log("valor do carro: " + porshe.valor)
+
+
+
+
